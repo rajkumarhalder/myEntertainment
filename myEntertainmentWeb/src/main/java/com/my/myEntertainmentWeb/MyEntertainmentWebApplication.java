8b8d6f2cf0f5b82af4623eea5ef31cdfcca0bb7c
rajkumr.halder <rajkumr.halder@rajkumrhalder>
2018-05-06 18:22:44 +0530

    1st Commit

---
+++ myEntertainmentWeb/src/main/java/com/my/myEntertainmentWeb/MyEntertainmentWebApplication.java
@@ -0,0 +1,12 @@
+package com.my.myEntertainmentWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEntertainmentWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEntertainmentWebApplication.class, args);
+	}
+}
